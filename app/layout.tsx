c8fa5bfb2856b820930b7b7a62e51a99c8eb7c62
SalsabeelZubaidi <158805325+SalsabeelZubaidi@users.noreply.github.com>
2026-02-02 10:00:40 +0300

    style: update app font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Space_Grotesk } from "next/font/google";
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],      
+  weight: ["400", "700"],  
+  variable: "--space-grotesk",
 });
 
 export const metadata: Metadata = {
@@ -24,8 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body 
+      className={`${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
